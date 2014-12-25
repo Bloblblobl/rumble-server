3918ea560aeda08ace0503d844100befd9aaf923
Bloblblobl <Bloblblobl@gmail.com>
2014-12-25 12:58:44 -0800

    Got rid of heroku-specific files

--- user.py
+++
@@ -1,7 +0,0 @@
-class User(object):
-    def __init__(self, username, password, handle, registered):
-        # List of invited members
-        self.username = username
-        self.password = password
-        self.handle = handle
-        self.registered = registered
