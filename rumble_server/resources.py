d2911f810e9ffb7585df17ae9c6373f4e69da85b
Bloblblobl <Bloblblobl@gmail.com>
2015-02-22 16:10:48 -0800

    Created a local test server

--- rumble_server/resources.py
+++ rumble_server/resources.py
@@ -6,14 +6,17 @@ from server import get_instance
 
 class User(Resource):
     def post(self):
-        parser = RequestParser()
-        parser.add_argument('username', type=str, required=True)
-        parser.add_argument('handle', type=str, required=True)
-        parser.add_argument('password', type=str, required=True)
-        args = parser.parse_args()
-        server = get_instance()
-        server.register(**args)
-        return dict(result='OK')
+        try:
+            parser = RequestParser()
+            parser.add_argument('username', type=str, required=True)
+            parser.add_argument('handle', type=str, required=True)
+            parser.add_argument('password', type=str, required=True)
+            args = parser.parse_args()
+            server = get_instance()
+            server.register(**args)
+            return dict(result='OK')
+        except Exception as e:
+            raise
 
 class Users(Resource):
     def get(self):
