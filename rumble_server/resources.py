afe395256ed32d2074e69fd5b56e16b004174937
Bloblblobl <Bloblblobl@gmail.com>
2015-03-07 18:23:56 -0800

    Added error handling to get_auth()

--- rumble_server/resources.py
+++ rumble_server/resources.py
@@ -3,6 +3,11 @@ from flask_restful import Resource, abort
 from flask_restful.reqparse import RequestParser
 from server import get_instance
 
+def get_auth():
+    user_auth = request.headers.get('Authorization', None)
+    if user_auth is None:
+        abort(401, message='Unauthorized user')
+    return user_auth
 
 class User(Resource):
     def post(self):
@@ -22,8 +27,7 @@ class Users(Resource):
     def get(self):
         if 'Authorization' not in request.headers:
             abort(401, message='Unauthorized user')
-        user_auth = request.headers['Authorization']
-
+        user_auth = get_auth()
         server = get_instance()
         return dict(result=server.get_users(user_auth=user_auth))
 
@@ -40,7 +44,7 @@ class ActiveUser(Resource):
 
     def delete(self):
         try:
-            user_auth = request.headers['Authorization']
+            user_auth = get_auth()
         except Exception as e:
             raise
         server = get_instance()
@@ -50,13 +54,13 @@ class ActiveUser(Resource):
 
 class RoomMember(Resource):
     def post(self, name):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         server.join_room(user_auth=user_auth, name=name)
         return dict(result='OK')
 
     def delete(self, name):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         server.leave_room(user_auth=user_auth, name=name)
         return dict(result='OK')
@@ -64,7 +68,7 @@ class RoomMember(Resource):
 
 class RoomMembers(Resource):
     def get(self, name):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         result = server.get_room_members(user_auth=user_auth, name=name)
         return dict(result=result)
@@ -72,13 +76,13 @@ class RoomMembers(Resource):
 
 class Room(Resource):
     def post(self, name):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         server.create_room(user_auth=user_auth, name=name)
         return dict(result='OK')
 
     def delete(self, name):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         server.destroy_room(user_auth=user_auth, name=name)
         return dict(result='OK')
@@ -86,14 +90,14 @@ class Room(Resource):
 
 class Rooms(Resource):
     def get(self):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         return dict(result=server.get_rooms(user_auth=user_auth))
 
 
 class Message(Resource):
     def post(self, name):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         parser = RequestParser()
         parser.add_argument('message', type=str, required=True)
         message = parser.parse_args()['message']
@@ -104,7 +108,7 @@ class Message(Resource):
 
 class Messages(Resource):
     def get(self, name, start, end):
-        user_auth = request.headers['Authorization']
+        user_auth = get_auth()
         server = get_instance()
         result = server.get_messages(user_auth, name, start, end)
         result = [(r[0].isoformat(), r[1], r[2]) for r in result]
