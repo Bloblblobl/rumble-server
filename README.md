e2a2a9fb886c7810030a303cdbf8e8104a06bb15
Saar Sayfan <saarsayfan@gmail.com>
2018-03-26 14:50:36 -0700

    Added deployment instructions for PythonAnywhere

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ Will Use **Flask-Restful** and **SQLite Database**
 
 [Project Map](https://docs.google.com/document/d/1pwb8R2YV4yL0URVyQtsfOuqbDk4sDiX6KIOshKuWdHQ)
 
+## PythonAnywhere Setup (from Scratch)
+1. Login to PythonAnywhere
+2. `git clone https://github.com/Bloblblobl/Rumble-Server.git `
+3. `cd Rumble-Server`
+4. `pip install --user -r requirements.txt`
+5. Replace WSGI configuration file with pythonanywhere_wsgi.py
+6. `cd rumble_server`
+7. `sqlite3 rumble.db < rumble_schema.sql`
+8. Reload app from dashboard
+
+### Refresh App on PythonAnywhere
 1. Login to PythonAnywhere
 2. Go to Bash Console
 3. Execute the Following Commands:
---
+++ pythonanywhere_wsgi.py
@@ -0,0 +1,18 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+
+# add your project directory to the sys.path
+project_home = u'/home/saarsayfan/Rumble-Server/rumble_server/'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+from api import create_app
+
+application = create_app(project_home + 'rumble.db')
